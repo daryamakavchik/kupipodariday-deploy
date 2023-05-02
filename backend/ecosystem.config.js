af5f2fe9fd4c037642cbafe3dbaf0cad81fcaece
daryamakavchik <darjamakavchik@gmail.com>
2023-05-01 18:32:11 -0700

    another variable fix

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,7 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require('dotenv').config({ path: __dirname + `/.env.deploy` });
 
-const { DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH } = process.env;
+const { DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF, DEPLOY_REPO } =
+  process.env;
 
 module.exports = {
   apps: [
@@ -21,6 +22,9 @@ module.exports = {
     production: {
       user: DEPLOY_USER,
       host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPO,
+      path: DEPLOY_PATH,
       'pre-deploy-local': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
       'post-deploy': 'cd backend && npm install && npm run build',
     },
