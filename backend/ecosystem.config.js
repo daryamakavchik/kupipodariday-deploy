0fb6be9c9c2afef9484d2cbd7fbb0d02cefce9a5
daryamakavchik <darjamakavchik@gmail.com>
2023-05-01 18:30:55 -0700

    fix variables

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require('dotenv').config({ path: __dirname + `/.env.deploy` });
 
-const { POSTGRES_USERNAME, POSTGRES_HOST, POSTGRES_PGDATA } = process.env;
+const { DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH } = process.env;
 
 module.exports = {
   apps: [
@@ -19,9 +19,9 @@ module.exports = {
 
   deploy: {
     production: {
-      user: POSTGRES_USERNAME,
-      host: POSTGRES_HOST,
-      'pre-deploy-local': `scp ./.env ${POSTGRES_USERNAME}@${POSTGRES_HOST}:${POSTGRES_PGDATA}`,
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      'pre-deploy-local': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
       'post-deploy': 'cd backend && npm install && npm run build',
     },
   },
