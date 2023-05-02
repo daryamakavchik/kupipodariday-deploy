cbcb65c58e32e537bc0e9df9ddc48c8c7ba82cb2
daryamakavchik <darjamakavchik@gmail.com>
2023-05-02 14:18:08 -0700

    fix docker compose yml

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+require('dotenv').config({ path: __dirname + `/.env.deploy` });
+
+const { DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF, DEPLOY_REPO } =
+  process.env;
+
+module.exports = {
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPO,
+      path: DEPLOY_PATH,
+      'post-deploy': 'docker-compose up -d',
+    },
+  },
+};
