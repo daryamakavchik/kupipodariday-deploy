7898fb1e4b57e1469214c9de0c61dea505f37d08
daryamakavchik <darjamakavchik@gmail.com>
2023-05-14 19:46:52 -0700

    path fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,7 +11,7 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
-      'pre-deploy-local': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'pre-deploy-local': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source`,
       'post-deploy': 'sudo docker compose up -d --pull --no-cache',
     },
   },
