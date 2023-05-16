086ffd986c55ab1b06b0756de4b5f3ace87c1076
daryamakavchik <darjamakavchik@gmail.com>
2023-05-16 09:57:42 -0700

    env example fix

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -1,13 +1,10 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-require('dotenv').config({ path: __dirname + `../.env` });
-
 import { Offer } from 'src/offers/entities/offer.entity';
 import { User } from 'src/users/entities/user.entity';
 import { Wish } from 'src/wishes/entities/wish.entity';
 import { Wishlist } from 'src/wishlists/entities/wishlist.entity';
 
 export default () => ({
-  port: parseInt(process.env.PORT, 10) || 8000,
+  port: parseInt(process.env.POSTGRES_PORT, 10) || 8000,
   database: {
     type: 'postgres',
     url: process.env.POSTGRES_HOST || 'database',
