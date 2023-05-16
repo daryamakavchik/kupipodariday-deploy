4fabe701061d47905d887edb797ccb62a4193922
daryamakavchik <darjamakavchik@gmail.com>
2023-05-16 09:32:35 -0700

    fix config

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config({ path: __dirname + `../.env` });
+
 import { Offer } from 'src/offers/entities/offer.entity';
 import { User } from 'src/users/entities/user.entity';
 import { Wish } from 'src/wishes/entities/wish.entity';
@@ -7,7 +10,7 @@ export default () => ({
   port: parseInt(process.env.PORT, 10) || 8000,
   database: {
     type: 'postgres',
-    url: process.env.DATABASE_URL || '127.0.0.1',
+    url: process.env.DATABASE_URL || 'localhost',
     port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
     username: process.env.DATABASE_USER || 'student',
     password: process.env.DATABASE_PASSWORD || 'student',
